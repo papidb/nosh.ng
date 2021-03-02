1d54468c7bfd335043981713eaae9d12694eda35
Daniel Benjamin <benjamindaniel706@gmail.com>
2021-03-02 13:15:07 +0100

    chore: added module resolver

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,25 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          action: './src/action',
+          assets: './src/assets',
+          constants: './src/constants',
+          components: './src/components',
+          navigation: './src/navigation',
+          screens: './src/screens',
+          hooks: './src/hooks',
+          helpers: './src/helpers',
+          services: './src/services',
+          styles: './src/styles',
+          utils: './src/utils',
+        },
+      },
+    ],
+    // ['@shopify/react-i18n/babel', {mode: 'from-dictionary-index'}],
+  ],
 };
