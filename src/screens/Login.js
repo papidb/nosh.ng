8d9ad1c7ed4627037363bbb48efa2a4524752dab
Daniel Benjamin <benjamindaniel706@gmail.com>
2021-03-02 13:58:13 +0100

    minor changes

---
+++ src/screens/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'components';
+
+const LoginScreen = () => {
+  return (
+    <View>
+      <Text>Login</Text>
+    </View>
+  );
+};
+
+export default LoginScreen;
