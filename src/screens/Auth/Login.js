fb418470279e60a8676dc557505918b52a929b62
Daniel Benjamin <benjamindaniel706@gmail.com>
2021-03-02 14:57:58 +0100

    feat: added navigation

---
+++ src/screens/Auth/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'components';
+
+const LoginScreen = () => {
+  return (
+    <View>
+      <Text>omo</Text>
+    </View>
+  );
+};
+
+export default LoginScreen;
