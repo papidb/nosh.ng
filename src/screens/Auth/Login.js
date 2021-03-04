3927ec0b8ed00d5ab8003eaec784c4fabc84c864
Daniel Benjamin <benjamindaniel706@gmail.com>
2021-03-04 14:29:13 +0100

    feat: added support for svg and scrollview input

--- src/screens/Auth/Login.js
+++ src/screens/Auth/Login.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import {Svg, Rect, Circle, Path} from 'react-native-svg';
+
 import {Box, Text} from 'components';
 
 export const Login = () => {
   return (
     <Box>
-      <Text color="primary">omosdsldkdlskdlsdkl</Text>
+      <Svg height="100" width="100">
+        <Rect x="0" y="0" width="100" height="100" fill="black" />
+        <Circle cx="50" cy="50" r="30" fill="yellow" />
+        <Circle cx="40" cy="40" r="4" fill="black" />
+        <Circle cx="60" cy="40" r="4" fill="black" />
+        <Path d="M 40 60 A 10 10 0 0 0 60 60" stroke="black" />
+      </Svg>
     </Box>
   );
 };
