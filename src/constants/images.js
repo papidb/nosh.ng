4bb88cbc80412aa680dc5f7e259dfc5f27e31c5b
Daniel Benjamin <benjamindaniel706@gmail.com>
2021-03-07 10:09:23 +0100

    added auth face to

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,5 +1,6 @@
 const images = {
   auth_logo: require('assets/images/auth_logo.png'),
+  auth_face: require('assets/images/auth_face.png'),
 };
 const toArray = () => {
   let keys = Object.keys(images);
