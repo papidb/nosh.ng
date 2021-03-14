74f4964bc9b4d1e60beef671137e105ee9177a68
Daniel Benjamin <benjamindaniel706@gmail.com>
2021-03-14 09:50:52 +0100

    added more util functions

--- src/shared/utils.js
+++ src/shared/utils.js
@@ -4,3 +4,20 @@ export const uuid = () => shortid.generate();
 
 export const waait = (amount = 0) =>
   new Promise((resolve) => setTimeout(resolve, amount));
+
+export function truncateString(string = '', maxLen = 25) {
+  return string.length > maxLen
+    ? string.substring(0, maxLen - 3) + '...'
+    : string;
+}
+
+export function getFirstLetter(string = '') {
+  return string.charAt(0);
+}
+export function capitalizeFirstLetter(string = '') {
+  string = String(string);
+  return getFirstLetter(string).toUpperCase() + string.slice(1);
+}
+
+export const generateReadableName = (user) =>
+  `${user.firstName} ${getFirstLetter(user.lastName).toUpperCase()}.`;
