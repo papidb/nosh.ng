3ee058ffbab809fda05706ae781e32b11e9ab0ab
Daniel Benjamin <benjamindaniel706@gmail.com>
2021-03-16 17:02:06 +0100

    added image for home screen

--- src/constants/images.js
+++ src/constants/images.js
@@ -2,6 +2,7 @@ const images = {
   auth_logo: require('assets/images/auth_logo.png'),
   auth_face: require('assets/images/auth_face.png'),
   itunes_2: require('assets/images/itunes2.png'),
+  FFHome: require('assets/images/FFHome.png'),
 };
 const toArray = () => {
   let keys = Object.keys(images);
