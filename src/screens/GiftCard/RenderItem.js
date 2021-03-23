83bbdab158304360adf1ca86d4622a6d8272deac
Daniel Benjamin <benjamindaniel706@gmail.com>
2021-03-23 14:16:17 +0100

    minor edits

--- src/screens/GiftCard/RenderItem.js
+++ src/screens/GiftCard/RenderItem.js
@@ -1,21 +1,13 @@
-import React, {useRef} from 'react';
-import {
-  ScrollView,
-  Image,
-  StyleSheet,
-  TouchableOpacity,
-  Dimensions,
-} from 'react-native';
+import React from 'react';
 import FastImage from 'react-native-fast-image';
 
-import {Box, Text, Divider, Button, HeaderInfo, Icon} from 'components';
+import {Box} from 'components';
 
 export const RenderItem = ({item: {title}}) => {
   const imageUri = `https://api.nosh.ng/uploads/images/cards/${title}.png`;
 
   return (
     <Box>
-      <Text>{title}</Text>
       <FastImage
         source={{uri: imageUri, priority: FastImage.priority.high}}
         style={{width: 300, height: 220}}
