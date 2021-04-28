a763e86432e545946b1fac1012710e852bf786a4
Daniel Benjamin <benjamindaniel706@gmail.com>
2021-04-28 21:42:16 +0100

    Remove username check on dev

--- src/selectors/user.selector.js
+++ src/selectors/user.selector.js
@@ -2,9 +2,8 @@ import {createSelector} from 'reselect';
 
 export const selectUser = (state) => state.user;
 
-export const selectHasUsername = createSelector(
-  selectUser,
-  (user) => !!user.username,
+export const selectHasUsername = createSelector(selectUser, (user) =>
+  __DEV__ ? true : !!user.username,
 );
 
 export const selectPureUser = createSelector(selectUser, (user) => user);
